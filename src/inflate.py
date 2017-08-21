b14014b68ae9f9d03dfa1d5c9b4d1bfa15ef0f8b
Yana Hasson <yanahasson@gmail.com>
2017-08-21 11:12:36 +0200

    Add kernel_size, padding, stride and dilation transfer to inflate_convolution

--- src/inflate.py
+++ src/inflate.py
@@ -1,14 +1,19 @@
 import torch
 from torch.nn.parameter import Parameter
 
-def inflate_convolution(conv2d, time_dim=3):
+
+def inflate_convolution(conv2d, time_dim=3, time_padding=0, time_stride=1, time_dilation=1):
     # To preserve activations, padding should be by continuity and not zero
     # or no padding in time dimension
-    padding = (0, 1, 1)
-    conv3d = torch.nn.Conv3d(conv2d.in_channels, conv2d.out_channels, time_dim, padding=padding)
+    kernel_dim = (time_dim, conv2d.kernel_size[0], conv2d.kernel_size[1])
+    padding = (time_padding, conv2d.padding[0], conv2d.padding[1])
+    stride = (time_stride, conv2d.stride[0], conv2d.stride[0])
+    dilation = (time_dilation, conv2d.dilation[0], conv2d.dilation[1])
+    conv3d = torch.nn.Conv3d(conv2d.in_channels, conv2d.out_channels,
+                             kernel_dim, padding=padding, dilation=dilation)
     # Repeat filter time_dim times along time dimension
     weight_3d = conv2d.weight.unsqueeze(2).repeat(1, 1, time_dim, 1, 1).data
-    weight_3d = weight_3d/time_dim
+    weight_3d = weight_3d / time_dim
 
     # Assign new params
     conv3d.weight = Parameter(weight_3d)
